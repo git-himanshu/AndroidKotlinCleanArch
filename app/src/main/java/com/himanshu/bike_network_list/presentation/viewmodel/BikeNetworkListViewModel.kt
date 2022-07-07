ac2746f1f66696079400a1bab0c7b6b89f9b0336
himgupta0 <himanshu.gupta@publicissapient.com>
2022-07-07 15:57:23 +0530

    Local search for the bike network implemented by caching the data in the viewmodel. Test cases for viewmodel also fixed.

--- app/src/main/java/com/himanshu/bike_network_list/presentation/viewmodel/BikeNetworkListViewModel.kt
+++ app/src/main/java/com/himanshu/bike_network_list/presentation/viewmodel/BikeNetworkListViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.himanshu.bike_network_list.domain.entity.BikeNetworkEntity
-import com.himanshu.bike_network_list.domain.usecase.FilterBikeNetworkListUseCase
 import com.himanshu.bike_network_list.domain.usecase.GetBikeNetworkListUseCase
 import com.himanshu.bike_network_list.presentation.model.BikeNetworkUI
 import com.himanshu.bike_network_list.presentation.model_mapper.BikeNetworkUIModelMapper
@@ -12,19 +11,21 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
+import java.util.*
 import javax.inject.Inject
 
 class BikeNetworkListViewModel @Inject constructor(
     private val useCase: GetBikeNetworkListUseCase,
-    private val filterListUseCase: FilterBikeNetworkListUseCase,
     private val modelMapper: BikeNetworkUIModelMapper,
 ) : ViewModel() {
 
     private val disposable = CompositeDisposable()
 
-    private val _bikeNetworkList = MutableLiveData<List<BikeNetworkUI>>()
-    val bikeNetworkList: LiveData<List<BikeNetworkUI>>
-        get() = _bikeNetworkList
+    private var _bikeNetworkListCached = listOf<BikeNetworkUI>()
+
+    private val _bikeNetworkUIList = MutableLiveData<List<BikeNetworkUI>>()
+    val bikeNetworkUIList: LiveData<List<BikeNetworkUI>>
+        get() = _bikeNetworkUIList
 
     private val _bikeNetworkListLoadError = MutableLiveData<Boolean>()
     val bikeNetworkListLoadError: LiveData<Boolean>
@@ -51,18 +52,25 @@ class BikeNetworkListViewModel @Inject constructor(
     }
 
     fun filterList(text: String) {
-        _loading.value = true
-        disposable.add(
-            filterListUseCase(text)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeWith(handleResponse())
-        )
+        if (text.isEmpty()) {
+            _bikeNetworkUIList.value = _bikeNetworkListCached
+        } else {
+            val filteredList = _bikeNetworkListCached.filter {
+                val networkName = it.name ?: ""
+                networkName.lowercase(Locale.getDefault()).startsWith(
+                    text.lowercase(
+                        Locale.getDefault()
+                    )
+                )
+            }
+            _bikeNetworkUIList.value = filteredList
+        }
     }
 
     private fun handleResponse() = object : DisposableSingleObserver<List<BikeNetworkEntity>>() {
         override fun onSuccess(value: List<BikeNetworkEntity>) {
-            _bikeNetworkList.value = value.map { modelMapper.fromEntityToUIModel(it) }
+            _bikeNetworkListCached = value.map { modelMapper.fromEntityToUIModel(it) }
+            _bikeNetworkUIList.value = _bikeNetworkListCached
             _bikeNetworkListLoadError.value = false
             _loading.value = false
         }
